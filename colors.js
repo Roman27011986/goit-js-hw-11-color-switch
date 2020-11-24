d12348443f8ee3ce332a24cc63b33ca2b1205911
Roman27011986 <>
2020-11-24 23:41:30 +0200

    qwe

--- colors.js
+++ colors.js
@@ -1,3 +1,7 @@
+const stertRef = document.querySelector('button[data-action="start"]');
+const stopRef = document.querySelector('button[data-action="stop"]');
+let timeoutId = null;
+let isActive = false;
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -6,6 +10,23 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const addbodyColor = () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  timeoutId = setInterval(() => {
+    let randomColor = randomIntegerFromInterval(0, colors.length);
+    document.body.style.background = colors[randomColor];
+  }, 1000);
+};
+stertRef.addEventListener('click', addbodyColor);
+stopRef.addEventListener('click', () => {
+  clearTimeout(timeoutId);
+  isActive = false;
+});
